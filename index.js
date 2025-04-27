f41141f2a9b501a6a919bfc020c78ee6240b1388
mahmud-r-farhan <mdmanna.2020.manna@gmail.com>
2025-04-27 15:31:16 +0600

    Just for making funny reels!

--- index.js
+++ index.js
@@ -5,36 +5,32 @@ import random from "random";
 
 const path = "./data.json";
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+const getRandomDate = () => {
+  // Start: Jan 1, 2022
+  const start = moment("2022-01-01");
+  // End: Dec 31, 2025
+  const end = moment("2025-03-17");
 
-  const data = {
-    date: date,
-  };
+  // Get a random number of days between start and end
+  const daysDiff = end.diff(start, 'days');
+  const randomDays = random.int(0, daysDiff);
 
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
-  });
+  // Add random days to start
+  return start.add(randomDays, 'days').format();
 };
 
 const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  if (n === 0) {
+    return simpleGit().push();
+  }
 
-  const data = {
-    date: date,
-  };
+  const date = getRandomDate();
+  const data = { date: date };
   console.log(date);
+
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+    simpleGit().add([path]).commit(date, { "--date": date }, makeCommits.bind(this, --n));
   });
 };
 
-makeCommits(100);
+makeCommits(1000); // You can change 300 to whatever number of commits you want
